d23204e76bc35796bb66e1e5797ccde8608c1d07
varshinibt07 <105836401+varshinibt07@users.noreply.github.com>
2022-07-28 00:07:10 +0530

    Assignment1

---
+++ Chocolate/src/com/xworkz/chocolate/kinderjoy/Vendor.java
@@ -0,0 +1,14 @@
+package com.xworkz.chocolate.kinderjoy;
+
+public class Vendor {
+	public Vendor()
+	{
+		System.out.println("Price is 50");
+	}
+	
+	
+	public String getName() {
+		return "Kinderjoy";
+	}
+
+}
