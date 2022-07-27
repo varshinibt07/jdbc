322e253beb9a8cc2a5aa9bce4f05511975656b4f
varshinibt07 <105836401+varshinibt07@users.noreply.github.com>
2022-07-28 00:02:05 +0530

    Assignment1

---
+++ Chocolate/src/com/xworkz/chocolate/kinderjoy/Kinderjoy.java
@@ -0,0 +1,15 @@
+package com.xworkz.chocolate.kinderjoy;
+
+public class Kinderjoy {
+	public static void main(String[] args) {
+		
+		System.out.println("Eat and play....!");
+		
+		
+		
+		Vendor vendor = new Vendor();
+		String vendor1=vendor.getName();
+		System.out.println(vendor1);
+	}
+
+}
---
+++ jar/src/com/xworkz/kissan/jam/Jam.java
@@ -0,0 +1,12 @@
+package com.xworkz.kissan.jam;
+
+public class Jam {
+	public static void main(String[] args) {
+		System.out.println("Running main method in jam");
+		
+		Vendor vendor = new Vendor();
+		String vendor1=vendor.getName();
+		System.out.println(vendor1);
+	}
+
+}
